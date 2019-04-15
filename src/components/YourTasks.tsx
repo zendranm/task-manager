53cef1aac4d166bcc60ac37be3790189c2851995
michal <zendran.m@gmail.com>
2019-04-15 12:28:20 +0200

    Geting subcollection tasks works

--- src/components/YourTasks.tsx
+++ src/components/YourTasks.tsx
@@ -1,10 +1,31 @@
 import * as React from 'react';
+import fire from '../../firebase';
 
 interface Props {
   match: any;
 }
 
 class YourTasks extends React.Component<Props> {
+  async componentDidMount() {
+    let db = fire.firestore();
+    await db
+      .collection('Lists')
+      .where('Name', '==', this.props.match.params.name)
+      .get()
+      .then((querySnapshot: any) => {
+        querySnapshot.forEach((doc: any) => {
+          doc.ref
+            .collection('Tasks')
+            .get()
+            .then((querySnapshot: any) => {
+              querySnapshot.forEach((doc: any) => {
+                console.log(doc.data());
+              });
+            });
+        });
+      });
+  }
+
   render() {
     return (
       <div>
@@ -15,6 +36,10 @@ class YourTasks extends React.Component<Props> {
         2.Dodać do bazy zadań taski
         <br />
         3.Dodać alerty przy usuwaninu czy na pewno
+        <br />
+        4.Przenieść metody odpytujące baze do jednego pliku
+        <br />
+        5.Uporządkować CSSy
       </div>
     );
   }
